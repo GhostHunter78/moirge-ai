a3f95c341d71e0ef1f0f115208f07d104fb72d05
Luka Talakvadze <lukatalakvadze@gmail.com>
2025-12-01 15:51:37 +0400

    fix: change google font globally

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -2,13 +2,7 @@ import type { Metadata } from "next";
 import { getMessages } from "next-intl/server";
 import { NextIntlClientProvider } from "next-intl";
 import "../../app/globals.css";
-import { Roboto } from "next/font/google";
 
-const roboto = Roboto({
-  subsets: ["latin"],
-  weight: ["300", "400", "500", "700"],
-  variable: "--font-roboto",
-});
 export const metadata: Metadata = {
   title: "Moirge AI",
   description: "AI powered ecommerce assistant",
@@ -19,16 +13,22 @@ export const metadata: Metadata = {
 
 export default async function RootLayout({
   children,
-  params
+  params,
 }: Readonly<{
   children: React.ReactNode;
-  params: Promise<{locale: string}>;
+  params: Promise<{ locale: string }>;
 }>) {
-  const {locale} = await params;
+  const { locale } = await params;
   const messages = await getMessages();
   return (
     <html lang={locale}>
-      <body className={`${roboto.variable} antialiased`}>
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+Georgian:wght@100..900&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body className="antialiased">
         <NextIntlClientProvider messages={messages}>
           {children}
         </NextIntlClientProvider>
